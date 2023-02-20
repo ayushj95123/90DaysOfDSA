006e5af70ebbd4e5e8a8d186dc2e7e75c553deca
AYUSH JAIN <ayushj95123@gmail.com>
2023-02-20 20:50:36 +0530

    Create MaxDiameter.py

---
+++ Trees/MaxDiameter.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class TreeInfo:
+    def __init__(self, height = 0, maxDiameter = 0):
+        self.height = height
+        self.maxDiameter = maxDiameter
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        treeInfo = self.getDiameter(root)
+        return treeInfo.maxDiameter
+
+    def getDiameter(self, root):
+        if root is None:
+            return TreeInfo()
+        
+        else:
+            leftInfo = self.getDiameter(root.left)
+            rightInfo = self.getDiameter(root.right)
+
+            currentHeight = max(leftInfo.height, rightInfo.height)+1
+
+            diameter = leftInfo.height + rightInfo.height
+            maxDiameter = max([diameter, leftInfo.maxDiameter, rightInfo.maxDiameter])
+
+            return TreeInfo(currentHeight, maxDiameter)
